a75f6bd867b4c81894bf7410968122c8c402cd2a
m0x <m0x@m0x.io>
2021-12-06 21:15:38 -0800

    Decided to handle Memory Mapping on the emulation side as an MMU (Memory Management Unit), even though the GameBoy didn't actually have one in terms of hardware, is makes implementing Memory Mapping easier.

--- pkg/mmu/mmu.go
+++ pkg/mmu/mmu.go
@@ -13,10 +13,41 @@
 					the GameBoy Emulator
 							m0x <3
 */
-package cpu
+package mmu
 
-import "gemu/pkg/mmu"
+/* https://gbdev.io/pandocs/Memory_Map.html
 
-type CPU struct {
-	mmu *mmu.MMU
+The Game Boy has a 16-bit address bus, which is used to address ROM, RAM, and I/O.
+
+Start	End		Description						Notes
+0000	3FFF	16 KiB ROM bank 00				From cartridge, usually a fixed bank
+4000	7FFF	16 KiB ROM Bank 01~NN			From cartridge, switchable bank via mapper (if any)
+8000	9FFF	8 KiB Video RAM (VRAM)			In CGB mode, switchable bank 0/1
+A000	BFFF	8 KiB External RAM				From cartridge, switchable bank if any
+C000	CFFF	4 KiB Work RAM (WRAM)
+D000	DFFF	4 KiB Work RAM (WRAM)			In CGB mode, switchable bank 1~7
+E000	FDFF	Mirror of C000~DDFF (ECHO RAM)	Nintendo says use of this area is prohibited.
+FE00	FE9F	Sprite attribute table (OAM)
+FEA0	FEFF	Not Usable						Nintendo says use of this area is prohibited
+FF00	FF7F	I/O Registers
+FF80	FFFE	High RAM (HRAM)
+FFFF	FFFF	Interrupt Enable register (IE)
+
+*/
+
+// MMU is the Memory Management Unit. While the GameBoy did not have an actual
+// MMU, it makes sense for our emulator. The GameBoy uses Memory Mapping to talk to
+// various subsystems. The MMU will be responsible for handling that mapping and will
+// be the only thing to actually access the memory directly.
+type MMU struct {
+	// The GameBoy has a memory map from 0x0000 - 0xFFFF
+	memory [0xFFFF]byte
 }
+
+// Initializes the MMU
+func (*MMU) init() {
+
+}
+
+// TODO: Write functions for reading and writing to memory, while handling and respecting
+// memory mapping rules
